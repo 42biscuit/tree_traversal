b09915f7acb3373ea7e1c018f19d30cdfd0a08eb
William Wood <72472543+42biscuit@users.noreply.github.com>
2022-02-11 13:57:31 +0000

    Add files via upload

--- src/node.rs
+++ src/node.rs
@@ -1,16 +1,16 @@
 pub type TreeIndex = usize;
-#[derive(Clone,Debug)]
+#[derive(Clone, Debug)]
 pub struct TreeNode {
     pub value: f32,
-    pub next_nodes: Box<Vec<Option<TreeIndex>>>,
-    pub weights: Box<Vec<f32>>,
+    pub next_nodes: Vec<Option<TreeIndex>>,
+    pub weights: Vec<f32>,
 }
 impl TreeNode {
     pub fn new(value: f32, next_nodes: Vec<Option<TreeIndex>>, weights: Vec<f32>) -> Self {
         TreeNode {
             value: value,
-            next_nodes: Box::new(next_nodes),
-            weights: Box::new(weights),
+            next_nodes: next_nodes,
+            weights: weights,
         }
     }
     ///sigmoid squishificaiton function to keep the avtivation values and weights between 0 and 1
@@ -19,12 +19,12 @@ impl TreeNode {
     }
 }
 
-impl activation_funcs<f32> for f32 {
+impl ActivationFuncs<f32> for f32 {
     fn sigmoid_squish(input: &f32) -> Option<f32> {
         Some(1.0 / (1.0 + 2.71828_f32.powf(*input)))
     }
 }
 
-trait activation_funcs<T> {
+trait ActivationFuncs<T> {
     fn sigmoid_squish(input: &T) -> Option<T>;
 }
